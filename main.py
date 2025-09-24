ff766b8ababd2c421d883d29f737a0bc014124e6
Anvitha N <nanvitha123@gmail.com>
2025-09-24 16:27:22 +0530

    Update main for render deployement.py

--- main.py
+++ main.py
@@ -72,5 +72,6 @@ def index():
 
 if __name__ == '__main__':
     import os
+    port = int(os.environ.get('PORT', 5000))
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host='0.0.0.0', port=5000, debug=debug_mode)
